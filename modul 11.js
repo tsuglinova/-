ee97ac890d9a42f94dfd094fc670fdf8b16c0c38
tsuglinova <162887001+tsuglinova@users.noreply.github.com>
2024-04-14 15:10:45 +0300

    Update modul 11.js

--- modul 11.js
+++ modul 11.js
@@ -1,1 +1,59 @@
+Задача 1
+const someArray = [0, 1, 2, 3, 4, 5, 6, 0, 8, 9, 10, 0, 't', null, NaN, true];
+
+function countEvenOddZeroElements(array) {
+    if (!Array.isArray(array)) {
+        console.log('Передайте массив');
+        return;
+    }
+
+    let countOdd = 0;
+    let countEven = 0;
+    let zeroElementsCount = 0;
+
+    array.forEach(item => {
+        if (typeof item === 'number' && !isNaN(item)) {
+            if (item === 0) {
+                zeroElementsCount++;
+            }
+            else if (item % 2 === 0) {
+                countEven++;
+            } else {
+                countOdd++;
+            }
+        }
+    });
+
+    return {
+        countOdd,
+        countEven,
+        zeroElementsCount
+    }
+}
+
+const result = countEvenOddZeroElements(someArray);
+
+console.log(`Колличество четных элементов: ${result.countEven}`);
+console.log(`Колличество нечетных элементов: ${result.countOdd}`);
+console.log(`Колличество нулевых элементов: ${result.zeroElementsCount}`);
+
+Задача 2 
+function isPrimeNumber(num) {
+    if ((num < 2) || (num > 1000)) {
+        return 'Данные не верны';
+    } else {
+        for (let i = 2; i < num; i++) {
+            if (num % i === 0) {
+                return 'Составное';
+            }
+        }
+        return 'Простое';
+    }
+}
+
+let randomNumber = Math.floor(Math.random() * 1000);
+let result = isPrimeNumber(randomNumber);
+console.log(`Число ${randomNumber} - ${result}`);
+
+Задача 3
 
